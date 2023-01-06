6a7c52f9e08143b52b704088d96b7da85fee3e66
pandyamarut <pandyamarut@gmail.com>
2023-01-06 11:42:29 -0600

    Updates

---
+++ component---src-pages-index-js-72f0d48e27819174439c.js
@@ -0,0 +1,2 @@
+(self.webpackChunkv1=self.webpackChunkv1||[]).push([[678],{7704:function(e,n,t){"use strict";t.r(n);var l=t(7294),a=t(9),c=t(7140),r=a.ZP.main.withConfig({displayName:"pages__StyledMainContainer",componentId:"sc-1ppsr5x-0"})(["counter-reset:section;"]);n.default=function(e){var n=e.location;return l.createElement(c.Ar,{location:n},l.createElement(r,{className:"fillHeight"},l.createElement(c.VM,null),l.createElement(c.CL,null),l.createElement(c.pe,null),l.createElement(c.r8,null)))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-72f0d48e27819174439c.js.map
